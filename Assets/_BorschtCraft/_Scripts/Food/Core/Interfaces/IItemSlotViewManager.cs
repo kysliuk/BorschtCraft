d74e6aa724f415601608daccff5791c8f0ea5e8a
kysliuk <dkysliuk@gmail.com>
2025-06-13 19:37:11 +0300

    fix dublicated items

--- Assets/_BorschtCraft/_Scripts/Food/Core/Interfaces/IItemSlotViewManager.cs
+++ Assets/_BorschtCraft/_Scripts/Food/Core/Interfaces/IItemSlotViewManager.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 using Zenject;
 using System;
 using System.Collections.Generic;
-using BorschtCraft.Food.UI; // For IManagedConsumedView, ConsumedViewModelMapping
+using BorschtCraft.Food.UI;
 
-namespace BorschtCraft.Food.Core.Interfaces
+namespace BorschtCraft.Food
 {
     public interface IItemSlotViewManager
     {
         void Initialize(Transform slotTransform, DiContainer diContainer, SignalBus signalBus, List<ConsumedViewModelMapping> viewModelMappings, Dictionary<Type, IManagedConsumedView> childViews);
         void DisplayItem(IConsumed itemToDisplay, IConsumed overallRootItem);
         void ClearView();
-        void OnDestroy(); // For potential cleanup
+        void OnDestroy();
     }
 }
