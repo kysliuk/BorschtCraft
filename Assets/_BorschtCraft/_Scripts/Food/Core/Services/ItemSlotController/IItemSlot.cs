c34023580b198455dc8e123aa4718f0980a8c6ce
kysliuk <dkysliuk@gmail.com>
2025-06-13 19:50:44 +0300

    1

--- Assets/_BorschtCraft/_Scripts/Food/Core/Services/ItemSlotController/IItemSlot.cs
+++ Assets/_BorschtCraft/_Scripts/Food/Core/Services/ItemSlotController/IItemSlot.cs
@@ -8,6 +8,6 @@ namespace BorschtCraft.Food
         bool TrySetItem(IConsumed newItem);
         IConsumed ReleaseItem();
         bool IsEmpty();
-        GameObject GetGameObject(); // Added for logging/context, as ItemSlotController is a MonoBehaviour
+        GameObject GetGameObject();
     }
 }
