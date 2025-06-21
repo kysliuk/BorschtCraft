a0719a881254d24ccc8033178f0d9b795ef82ae2
kysliuk <dkysliuk@gmail.com>
2025-06-21 17:01:22 +0300

    change directory

---
+++ Assets/_BorschtCraft/_Scripts/Food/Core/ConsumeAbstractFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BorschtCraft.Food
+{
+    public static class ConsumeAbstractFactory
+    {
+        public static T CreateConsumed<T>(int price, IConsumed item) where T : IConsumed
+        {
+            return (T)Activator.CreateInstance(typeof(T), price, item);
+        }
+
+        public static T CreateConsumable<T>(int price) where T : Item
+        {
+            return (T)Activator.CreateInstance(typeof(T), price);
+        }
+    }
+}
